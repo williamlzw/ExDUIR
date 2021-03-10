375302454f5242ce86db80600c51e2550f60ddbf
huadream <admin@hjcns.com>
2021-03-10 23:48:16 +0800

    clean

--- Exdui5/Class_Static_SysLink_ex.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "help_ex.h"
-#include "Object_ex.h"
-
-void _static_paint(EXHANDLE hObj, obj_s* pObj);
-size_t _static_proc(HWND hWnd, EXHANDLE hObj, int uMsg, size_t wParam, size_t lParam, obj_s* pObj);
--- Exdui5/Class_SysButton_Page_ex.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "help_ex.h"
-
-size_t _sysbutton_paint(HWND hWnd, size_t hObj, obj_s* pObj);
-void _sysbutton_remove_proc(obj_s* pObj, int width, int height);
-size_t _sysbutton_proc(HWND hWnd, size_t hObj, int uMsg, size_t wParam, size_t lParam, obj_s* pObj);
-size_t _page_proc(HWND hWnd, size_t hObj, int uMsg, size_t wParam, size_t lParam, obj_s* pObj);
-void _page_onvscrollbar(HWND hWnd, size_t hObj, obj_s* pObj, int uMsg, size_t wParam, size_t lParam);
-size_t _page_paint(size_t hObj);
