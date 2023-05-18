c466f5b1a808d99add0e13aeabf127eece1b7338
william_lzw <313620225@qq.com>
2023-05-18 11:04:46 +0800

    update

--- msvc/test/test_obj.cpp
+++ msvc/test/test_obj.cpp
@@ -138,21 +138,30 @@ void test_button(HWND hWnd)
 
 void test_label(HWND hWnd)
 {
-	HWND hWnd_label = Ex_WndCreate(hWnd, L"Ex_DirectUI", L"测试标签", 0, 0, 400, 400, 0, 0);
+	HWND hWnd_label = Ex_WndCreate(hWnd, L"Ex_DirectUI", L"测试基础组件", 0, 0, 400, 400, 0, 0);
 	HEXDUI hExDui_label = Ex_DUIBindWindowEx(hWnd_label, 0, EWS_NOINHERITBKG | EWS_BUTTON_CLOSE | EWS_BUTTON_MIN | EWS_MOVEABLE | EWS_CENTERWINDOW | EWS_TITLE | EWS_HASICON | EWS_NOSHADOW, 0, 0);
 	Ex_DUISetLong(hExDui_label, EWL_CRBKG, ExARGB(150, 150, 150, 255));
 	HEXOBJ hObj_label = Ex_ObjCreateEx(-1, L"static", NULL, EOS_BORDER | EOS_VISIBLE, 10, 30, 180, 150, hExDui_label, 0, DT_VCENTER, 0, 0, NULL);
 	std::vector<CHAR> imgdata;
 	Ex_ReadFile(L"res/Loading.gif", &imgdata);
 	Ex_ObjSetBackgroundImage(hObj_label, imgdata.data(), imgdata.size(), 0, 0, BIR_DEFAULT, 0, BIF_PLAYIMAGE, 255, TRUE);
+
 	Ex_ObjSetRadius(hObj_label, 10, 10, 15, 10, TRUE);
+
+
+
 	EX_BACKGROUNDIMAGEINFO bkg{ 0 };
 	Ex_ObjGetBackgroundImage(hObj_label, &bkg);
 	OUTPUTW(L"背景信息:", bkg.x, bkg.y, bkg.dwAlpha, bkg.dwRepeat, bkg.hImage, bkg.curFrame, bkg.maxFrame);
 	RECT rect;
 	Ex_ObjGetRect(hObj_label, &rect);
 	OUTPUTW(L"标签矩形:", rect.right, rect.bottom);
-	HEXOBJ hObj_label4 = Ex_ObjCreateEx(-1, L"static", L"标签可以填充动画,支持PNG,GIF,JPG,BMP,标签可以自动换行", -1, 200, 200, 180, 90, hExDui_label, 0, DT_WORDBREAK, 0, 0, NULL);
+	
+	HEXOBJ hObj_label2 = Ex_ObjCreateEx(-1, L"static", NULL, EOS_BORDER | EOS_VISIBLE, 200, 30, 180, 150, hExDui_label, 0, DT_VCENTER, 0, 0, NULL);
+	Ex_ReadFile(L"res/409.dds", &imgdata);
+	Ex_ObjSetBackgroundImage(hObj_label2, imgdata.data(), imgdata.size(), 0, 0, BIR_DEFAULT, 0, BIF_PLAYIMAGE, 255, TRUE);
+	
+	HEXOBJ hObj_label4 = Ex_ObjCreateEx(-1, L"static", L"基础组件可以填充动画,支持PNG,GIF,JPG,BMP,DDS,ICO,标签可以自动换行", -1, 200, 200, 180, 90, hExDui_label, 0, DT_WORDBREAK, 0, 0, NULL);
 	Ex_ObjSetFontFromFamily(hObj_label4, L"宋体", 14, EFS_BOLD, FALSE);
 	Ex_ObjSetColor(hObj_label4, COLOR_EX_TEXT_NORMAL, ExARGB(133, 33, 53, 255), TRUE);
 
